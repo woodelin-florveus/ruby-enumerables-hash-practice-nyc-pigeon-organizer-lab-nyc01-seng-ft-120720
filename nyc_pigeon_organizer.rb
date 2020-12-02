7ab2ef458f813fdb479fc1fff9264f40e06f7e8b
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-02 20:45:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |first, names|
+      names.each do |name|
+        # create names pointing to a hash and its attributes
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][key]
+            new_hash[name][key] = []
+        end
+        # binding.pry
+        new_hash[name][key] << first.to_s
+      end
+    end
+  end
+  new_hash
 end
